8a5fa9233f609346da718d2cb98b5a24f58bd443
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-02-13 00:01:34 +0530

    added form validation

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -5,9 +5,31 @@ import OldMan from "@/asserts/oldman.png";
 import { Button, InputAdornment, TextField } from "@mui/material";
 import RemoveRedEyeOutlinedIcon from "@mui/icons-material/RemoveRedEyeOutlined";
 import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type FormVlaue = {
+  email: string;
+  password: string;
+};
 
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<FormVlaue>();
+
+  const [data, setData] = useState({ email: "", password: "" });
+
+  const onSubmit: SubmitHandler<FormVlaue> = (data) => {
+    console.log("final data", data);
+    // alert(data.email);
+    setData({ email: "", password: "" });
+    reset();
+  };
 
   const toggleShowPassword = () => {
     setShowPassword(!showPassword);
@@ -16,16 +38,10 @@ const Login = () => {
   return (
     <div className="flex">
       <div className="flex-1">
-        <Image
-          src={OldMan}
-          alt=""
-          width={500}
-          height={700}
-          className="h-screen w-full object-center"
-        />
+        <Image src={OldMan} alt="" width={500} height={700} />
       </div>
       <div className="flex-1 flex justify-center items-center">
-        <form className="w-[430px] h-[545px]">
+        <form className="w-[430px] h-[545px]" onSubmit={handleSubmit(onSubmit)}>
           <div className="h-[144px]">
             <h1 className="font-normal text-[40px]">
               Welcome Back <br /> to{" "}
@@ -45,7 +61,19 @@ const Login = () => {
                   required
                   id="eamil"
                   type="text"
+                  {...register("email", {
+                    required: "Email is required",
+                    pattern: {
+                      value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                      message: "Invalid Email",
+                    },
+                  })}
                 />
+                {errors.email && (
+                  <p className="error-msg text-red-500">
+                    {errors.email.message}
+                  </p>
+                )}
               </div>
               <div className="flex flex-col gap-1">
                 <label htmlFor="password" className="text-lg">
@@ -70,7 +98,32 @@ const Login = () => {
                       </InputAdornment>
                     ),
                   }}
+                  {...register("password", {
+                    required: "Password id required",
+                    minLength: {
+                      value: 10,
+                      message: "password should be atleast 10 char",
+                    },
+                    validate: (value) => {
+                      const containUpperCase = /[A-Z]/.test(value);
+                      const contaiLowerCase = /[a-z]/.test(value);
+                      const containsSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(
+                        value
+                      );
+                      return (
+                        (contaiLowerCase &&
+                          containUpperCase &&
+                          containsSpecialChar) ||
+                        "Password must contain at least one uppercase letter, one lowercase letter, and one special character"
+                      );
+                    },
+                  })}
                 />
+                {errors.password && (
+                  <p className="error-msg text-red-500">
+                    {errors.password.message}
+                  </p>
+                )}
               </div>
             </div>
             <div className="flex justify-end my-2">
@@ -84,7 +137,7 @@ const Login = () => {
                 color="primary"
                 variant="contained"
                 fullWidth
-                className="text-white bg-blue-700  font-semibold  text-xl rounded-lg py-3">
+                className="text-white bg-blue-600  font-semibold  text-xl rounded-lg py-3">
                 LOG IN
               </Button>
             </div>
